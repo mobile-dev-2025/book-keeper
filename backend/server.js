e83e76d6d8daf5f77af5d8eb80e19e43bbd9ed73
Gloryozo <gloryozoji@gmail.com>
2025-03-21 08:51:07 +0200

    Added environment comment

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 const port = 8000;
 
+// Load environment variables from .env file
 require("dotenv").config();
 
 app.use(express.json());
